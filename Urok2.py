84571023768fc8fec3cff9911c04e6cbac57cd2d
tegeran87 <mtv-rd@mail.ru>
2023-10-23 20:32:59 +0300

    first commit

---
+++ Urok2.py
@@ -0,0 +1,9 @@
+def multiplication_table(n):
+    for i in range(1, n + 1):
+        for j in range(1, 10):
+            print(f"{i} * {j} = {i * j}")
+        print('')
+
+
+n = int(input("Введите число n: "))
+multiplication_table(n)
